bda9d03240c9c2eb633688281317205a404ab58e
Sophie Popow <sophie.popow@gmail.com>
2020-06-27 20:06:10 +0200

    BFS Exercise

--- Graphs/7b_1_kapitanStatku.py
+++ Graphs/7b_1_kapitanStatku.py
@@ -1,3 +1,5 @@
+#Kapitan pewnego statku zastanawia się, czy może wpłynąć do portu mimo, że nastąpił odpływ. Do dyspozycji ma mapę zatoki w postaci tablicy M, gdzie M[y][x] to głebokość zatoki na pozycji (x,y). Jeśli jest ona większa niż pewna wartość int T to statek może się tam znaleźć. Początkowo statek jest na pozycji (0, 0) a port znajduje się na pozycji (n − 1, m − 1). Z danej pozycji statek może przepłynąć bezpośrednio jedynie na pozycję bezpośrednio obok (to znaczy, na pozycję, której dokładnie jedna ze współrzędnych różni się o jeden). Proszę napisać funkcję rozwiązującą problem kapitana.
+
 def BFS(M,n,m,T):
 
     def check(x, y):
