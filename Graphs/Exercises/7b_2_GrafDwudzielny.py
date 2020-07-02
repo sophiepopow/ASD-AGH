a3c4427baf174e147a23d199f25538a3184c9f80
Sophie Popow <sophie.popow@gmail.com>
2020-07-02 13:24:44 +0200

    Algorithm that checks if undirected graph is bipartite

---
+++ Graphs/Exercises/7b_2_GrafDwudzielny.py
@@ -0,0 +1,23 @@
+def DFSHelper(G,i,colors,parentColor,visited):
+    for j in range(len(G[i])):
+        if G[i][j] and visited[j] == False:
+            visited[j] = True
+            if colors[j] and colors[j] == colors[i]:
+                return False
+            colors[j] = not colors[i]
+            if not DFSHelper(G, j, colors, colors[j], visited)
+                return False
+    return True
+
+def DFSWithColors(G):
+    colors = [None]*len(G) 
+    visited = [False]* len(G)
+    visited[0]= True
+
+    for i in range(len(G)):
+        if not visited[i]:
+            visited[i]= True
+            colors[i] = True
+            if not DFSHelper(G,i,colors, colors[i], visited):
+                return False
+    return True
