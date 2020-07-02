bc18ff25e2d123b8316101e96a3484d85964e93d
Sophie Popow <sophie.popow@gmail.com>
2020-07-02 15:06:43 +0200

    Algorithm that checks if undirected graph has cycle

--- Graphs/Exercises/7b_2_CyklNieskierowany.py
+++ Graphs/Exercises/7b_2_CyklNieskierowany.py
@@ -3,7 +3,7 @@
 def DFSHelper(G,i,visited, branch):
     
     for j in range(len(G[i])):
-        if branch[j]:
+        if G[i][j] and branch[j]:
             return False
 
         if G[i][j] and visited[j] == False:
@@ -17,7 +17,6 @@ def DFSHelper(G,i,visited, branch):
 
 def DFSCycle(G):
     visited = [False]* len(G)
-    visited[0]= True
 
     for i in range(len(G)):
         if not visited[i]:
@@ -27,17 +26,21 @@ def DFSCycle(G):
                 return False
     return True
 
+# Doesnt work :(
+# def DFSCycle2(G):
+# 	visited = [False] * len(G)
+# 	def Traverse(par =-1,i=0):
+# 		visited[i] = True
+# 		for neighbour in G[i]:
+# 				if neighbour == par:
+# 					continue
+# 				if visited[neighbour]:
+# 					return True
+# 				Traverse(i,neighbour)
+# 		visited[i] = False
+# 		return False
+# 	return Traverse()
 
-def DFSCycle2(G):
-	visited = [False] * len(G)
-	def Traverse(par =-1,i=0):
-		visited[i] = True
-		for neighbour in G[i]:
-				if neighbour == par:
-					continue
-				if visited[neighbour]:
-					return True
-				Traverse(i,neighbour)
-		visited[i] = False
-		return False
-	return Traverse()
+G2 = [[1, 7], [0, 2, 6, 5], [1, 3, 4, 6], [2, 4], [2, 3, 5, 6], [1, 4, 6, 8], [1, 2, 4, 5], [0, 8], [7, 5]]
+G = [[1],[0,2,4,5], [1,3], [2], [1,5], [1,4]]
+print(DFSCycle(G))
